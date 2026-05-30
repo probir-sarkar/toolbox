b98fd56b23bedba13b44708c1195817394539409
Probir Sarkar <probirsarkar855@gmail.com>
2026-05-30 15:32:58 +0530

    added support for nitro

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   resolve: {
@@ -13,7 +14,6 @@ export default defineConfig({
     port: 3000
   },
   plugins: [
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
     tailwindcss(),
     tanstackStart({
       srcDirectory: "./src",
@@ -27,6 +27,7 @@ export default defineConfig({
         host: "https://toolbox.probir.dev"
       }
     }),
+    nitro(),
     viteReact()
   ]
 });
